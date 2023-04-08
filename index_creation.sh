7ae4556ae5514b39f64f3ca512c2b0b3b836eb99
Gracie Gumm-Undergraduate Student at Vanderbilt University <gracie.e.gumm@vanderbilt.edu>
2023-04-08 12:32:43 -0500

    Update index_creation.sh

--- index_creation.sh
+++ index_creation.sh
@@ -2,6 +2,10 @@
 files=`ls -R localfiles/` #will need to be updated to pi folder
 file_path=localfiles #will need to be updated to pi folder
 FILE=$file_path/log.csv
+
+# The if/else block below will check if the log file has already
+# been created. If not, it will create it. If it exists, it will
+# do nothing.
 if [ -f "$FILE" ]; then
     echo "$FILE exists..."
 else
