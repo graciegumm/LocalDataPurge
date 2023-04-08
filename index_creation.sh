1b4d3b5ea2b58c06e3102e401f96a06c68044c25
Gracie Gumm-Undergraduate Student at Vanderbilt University <gracie.e.gumm@vanderbilt.edu>
2023-04-08 17:17:16 -0500

    Update index_creation.sh

--- index_creation.sh
+++ index_creation.sh
@@ -1,14 +1,22 @@
-#! /bin/bash
-files=`ls -R localfiles/` #will need to be updated to pi folder
-file_path=localfiles #will need to be updated to pi folder
+#!/bin/bash
+
+file_path=localfiles
 FILE=$file_path/log.csv
 
-# The if/else block below will check if the log file has already
-# been created. If not, it will create it. If it exists, it will
-# do nothing.
+# Create the index (log.csv) file if it doesn't already exist
 if [ -f "$FILE" ]; then
-    echo "$FILE exists..."
+  echo "$FILE exists..."
 else
-    echo "host, data, checkout" >> $file_path/log.csv
-    echo "Creating index file (log.csv)..."
+  echo "host, data, checkout" >> "$FILE"
+  echo "Creating index file (log.csv)..."
 fi
+
+# Loop over each file on the RPI
+for file in "$file_path"/*; do
+  if [[ "$(basename "$file")" == "log.csv" ]]; then
+    echo "Skipping file $file ..."
+    continue
+  fi
+  # Perform an action on the file
+  echo "Processing file $file ..."
+done
