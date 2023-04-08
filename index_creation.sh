9369b9e14e753c7ed1a4ccf87faa167fa19cd4ff
Gracie Gumm-Undergraduate Student at Vanderbilt University <gracie.e.gumm@vanderbilt.edu>
2023-04-08 12:30:50 -0500

    Update index_creation.sh

--- index_creation.sh
+++ index_creation.sh
@@ -3,7 +3,8 @@ files=`ls -R localfiles/` #will need to be updated to pi folder
 file_path=localfiles #will need to be updated to pi folder
 FILE=$file_path/log.csv
 if [ -f "$FILE" ]; then
-    echo "$FILE exists."
+    echo "$FILE exists..."
 else
     echo "host, data, checkout" >> $file_path/log.csv
+    echo "Creating index file (log.csv)..."
 fi
